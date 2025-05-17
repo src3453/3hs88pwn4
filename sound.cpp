d7d6730b29d2e04334b63f380d52f8d0b84e0ca8
src3453 <info@example.com>
2025-05-18 01:33:11 +0900

    a

--- sound.cpp
+++ sound.cpp
@@ -67,10 +67,6 @@ public:
         S3HS_SAMPLE_FREQ = sr;
     }
 
-    void setSampleRate(float sr) {
-        S3HS_SAMPLE_FREQ = sr;
-    }
-
     float sind(float theta) {
         return sin((float)theta*2*M_PI);
     }
